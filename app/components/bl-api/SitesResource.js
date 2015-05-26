0aae39a3cb4ee1b40bb79e0259b4b93df518a5dc
Andy Warren <andrew.warren@spectrakey.co.uk>
2015-05-27 00:57:14 +0100

    added site, post and user resources

---
+++ app/components/bl-api/SitesResource.js
@@ -0,0 +1,20 @@
+"use strict";
+/**
+ * Factory which provides actions to perform on Sites resource end point
+ * @class SitesResource
+ **/
+angular.module("blPrototype.api.sites", ["ngResource"]).factory("SitesResource", [
+    "$resource",
+    "env",
+    /**
+     * @constructor
+     * @param $resource {Service} angular resource service xhr wrapper for REST api's
+     * @param ENV {Object} API address'
+     **/
+    function($resource, env) {
+
+        return $resource(
+            env.API_ADDRESS + "sites/:domain"
+        );
+    }
+]);
