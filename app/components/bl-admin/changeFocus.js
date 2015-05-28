01e8cae990b4986bd11f366d13573e72a2c541de
Andy Warren <andrew.warren@spectrakey.co.uk>
2015-05-28 11:59:53 +0100

    added changeFocus functionality detects keypress '@' on email field and moves focus to password

--- app/components/bl-admin/changeFocus.js
+++ app/components/bl-admin/changeFocus.js
@@ -7,15 +7,44 @@ angular.module("bl.changeFocus", [
 ])
 /**
  * @constructor
+ * @example sets up a relationship between three elements using the id selector
+ * First element will detect the keypress (@) and move focus to the second element focus will then move onto the third element
+ * Works best with one input and a second readonly input
+ * bl-change-focus="{first-element}, {second-element}"
+ * <input id="{first-element}">
+ * <input id="{second-element}" ng-readonly=true>
+ * <input id="{third-element}">
  * @class blChangeFocus
  */
 .directive("blChangeFocus",[
-    function (){
+    '$interval',
+    function ($interval){
         return {
             restrict: "A",
-            require: "ngModel",
-            link: function($scope, $element, attrs, ctrl){
+            link: function($scope, $element, attrs ){
 
+                var inputIdsArr = attrs.blChangeFocus.split(', '),
+                    inputId = "#" + inputIdsArr[0],
+                    focusToId = "#" + inputIdsArr[1],
+                    passwordId = "#" + inputIdsArr[2];
+
+                var inputElement = $element.find(inputId);
+                var focusToElement = $element.find(focusToId);
+                var passwordElement = $element.find(passwordId);
+
+                inputElement.on('keydown', function(event){
+                    if (event.keyCode === 192 && event.shiftKey === true) {
+
+                        focusToElement.focus();
+                        focusToElement.css({color:'green'});
+
+                        var moveToPassword = $interval(function(){
+                            focusToElement.css({color:'black'});
+                            passwordElement.focus();
+                            $interval.cancel(moveToPassword);
+                        }, 2000);
+                    }
+                });
             }
         };
     }
