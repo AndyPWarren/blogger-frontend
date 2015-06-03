51d61d39cdbd87e7c9ba01d0c616a53bf34bce01
Andy Warren <andrew.warren@spectrakey.co.uk>
2015-06-03 17:52:16 +0100

    ui-view logic for header

--- app/components/bl-header/header.js
+++ app/components/bl-header/header.js
@@ -4,8 +4,10 @@
  * @author   SpectraKey
  */
 angular.module("blPrototype.header", [
-    "blPrototype.header.HeaderService"
+    "blPrototype.header.HeaderService",
+    "ui.router"
 ])
+
 
 /**
  * @example
@@ -13,49 +15,46 @@ angular.module("blPrototype.header", [
  * @constructor
  * @class blHeader
  */
-.directive("blHeader", [function (){
-        return {
-            restrict: "E",
-            replace: true,
-            templateUrl: "components/bl-header/header.html",
-            controller: "blHeaderCtrl",
-            link: function(scope){
-                /**
-                 * @method setHeader
-                 * @param  {Event}  $event  Angular event object
-                 * @param  {String} title
-                 */
-                var setHeader = function setHeader($event, values){
-                    scope.heading = values.heading;
-                    scope.tagline = values.tagline;
-                    scope.hero = values.hero;
-                };
-
-                scope.$on("blHeader", setHeader);
-            }
-        };
-    }
-])
+//.directive("blHeader", [function (){
+//        return {
+//            restrict: "E",
+//            replace: true,
+//            templateUrl: "components/bl-header/header.html",
+//            controller: "blHeaderCtrl",
+//            link: function(scope){
+//                /**
+//                 * @method setHeader
+//                 * @param  {Event}  $event  Angular event object
+//                 * @param  {String} title
+//                 */
+//                var setHeader = function setHeader($event, values){
+//                    scope.heading = values.heading;
+//                    scope.tagline = values.tagline;
+//                    scope.hero = values.hero;
+//                };
+//
+//                scope.$on("blHeader", setHeader);
+//            }
+//        };
+//    }
+//])
 
 .controller("blHeaderCtrl", [
+    "$rootScope",
     "$scope",
     "$state",
-    "UserFactory",
     "HostFactory",
-    function($scope, $state, UserFactory, HostFactory){
+    "UserFactory",
+
+    function($rootScope, $scope, $state, HostFactory, UserFactory){
 
         $scope.logout = function(){
             UserFactory.logout().then(function(){
-               $state.go("posts")
+                $state.go("app")
             });
         };
 
-        UserFactory.current().then(function(res){
-            $scope.user = res.data.user
-        })
         $scope.host = HostFactory.domain;
-
-
     }
 ])
 
